d1d495c32227be876f13ce0ac0d0f314c1d91545
Jorge Andrei Hernandez Rodriguez <jorgeandreyhernandez26@gmail.com>
2025-11-21 17:32:53 -0500

    Reetructura del archivo main y el login

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -2,9 +2,8 @@ from sqlalchemy import Column, Integer, String
 from app.databases.db import Base
 
 class UserDB(Base):
-    __tablename__ = "users"
+    __tablename__ = "usuarios"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    nickname = Column(String, unique=True, index=True)
+    password = Column(String)  # Guardarás hash aquí
